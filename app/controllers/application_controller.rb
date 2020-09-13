bd1caa7190479310a44872ecbfc01275120258cd
shi0280 <sho_ko504128@yahoo.co.jp>
2020-09-13 13:20:12 +0900

    add-devise-strong-parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
+  before_action :configure_permitted_parameters, if: :devise_controller?  
 
   private
 
@@ -13,4 +14,22 @@ class ApplicationController < ActionController::Base
         password == Rails.application.credentials[:basic_auth][:pass]
     end
   end
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [ :nickname,
+                    :last_name,
+                    :first_name,
+                    :last_name_kana,
+                    :first_name_kana,
+                    :birthday,
+                    :image,
+                    :profile
+                  ]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+  end
+
 end
