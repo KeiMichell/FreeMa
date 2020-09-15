2171a3da49c44ec92814f1bf36bfa0ba2f84e886
shi0280 <sho_ko504128@yahoo.co.jp>
2020-09-15 19:02:31 +0900

    edit-user-validation

--- app/models/address.rb
+++ app/models/address.rb
@@ -8,7 +8,7 @@ class Address < ApplicationRecord
   # Validation_definition
   VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
   VALID_KANA_REGEX = /\A[ぁ-んー－]+\z/
-  VALID_NUM_REGEX = /\A[0-9]+\z/
+  VALID_POSTCODE_REGEX = /\A\d{3}[-]\d{4}$|^\d{3}[-]\d{2}$|^\d{3}\z/
 
   # Registrations_validation
   validates :last_name, presence: true, length: { maximum: 30 },
@@ -20,7 +20,7 @@ class Address < ApplicationRecord
   validates :first_name_kana, presence: true, length: { maximum: 30 },
             format: { with: VALID_KANA_REGEX, message: "全角ひらがなで入力してください" }
   validates :postcode, presence: true, length: { maximum: 8 },
-            format: { with: VALID_NUM_REGEX, message: "半角数字で入力してください" }
+            format: { with: VALID_POSTCODE_REGEX, message: "半角数字と半角ハイフンで入力してください" }
   validates :prefecture_id, presence: true
   validates :city, presence: true
   validates :block, presence: true
