93cfd9007c90eaab27c9716969b9a7d3fc474f54
KeiMichell <krockdale611@gmail.com>
2020-09-18 11:44:36 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'product_detail/show'
+  resources :product_detail, only: [:show]
   root 'items#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
