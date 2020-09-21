9bb87c29d247c84fd56bc0af0366530271449af0
shi0280 <sho_ko504128@yahoo.co.jp>
2020-09-21 17:23:32 +0900

    edit-routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :product_detail, only: [:show]
+  root 'items#index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users, controllers: {
     registrations: 'users/registrations',
   }
@@ -6,6 +9,4 @@ Rails.application.routes.draw do
     get 'addresses', to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
   end
-  root 'items#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
