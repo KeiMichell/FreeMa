b45c76ed0431bf3d52fb57ab393dad7877e72a8a
shi0280 <sho_ko504128@yahoo.co.jp>
2020-09-25 05:49:30 +0900

    edit-model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   has_many :images
-  belongs_to :user
+  belongs_to :seller, class_name: 'User', foreign_key: 'seller_id'
+  belongs_to :buyer, class_name: 'User', optional: true, foreign_key: 'buyer_id'
   belongs_to :category
   belongs_to :brand
   belongs_to_active_hash :condition
