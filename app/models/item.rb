c2864fcfa23c4c8c18921195430dae7376cb3414
KeiMichell <krockdale611@gmail.com>
2020-09-24 17:57:40 +0900

    error update

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
   belongs_to :user
   belongs_to :category
   belongs_to :brand
-  belongs_to_active_hash :conditon
+  belongs_to_active_hash :condition
   belongs_to_active_hash :delivery_fee
   belongs_to_active_hash :delivery_day
   belongs_to_active_hash :prefecture
