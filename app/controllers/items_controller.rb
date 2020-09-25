914b54fc07c8e69dd9d7bc5aa353065a7b1aa887
shi0280 <sho_ko504128@yahoo.co.jp>
2020-09-26 06:08:00 +0900

    edit- controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,9 +7,17 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+    @item.images.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -20,4 +28,11 @@ class ItemsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :detail, :category_id, :brand_id, :price, :condition_id, :delivery_fee_id, :prefecture_id, :delivery_day_id, images_attributes: [:image]).merge(seller_id: current_user.id)
+  end
+
 end
---
+++ app/models/delivery_day.rb
@@ -0,0 +1,5 @@
+class DeliveryDay < ActiveHash::Base
+  self.data = [
+      { id: 1, name: '１〜2日で配送' }, { id: 2, name: '2〜3日で配送' }, { id: 3, name: '4〜7 日で配送' }
+  ]
+end 
