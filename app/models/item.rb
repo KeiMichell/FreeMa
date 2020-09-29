7f800111dbd0cb1f450541385212ce0253759644
KeiMichell <krockdale611@gmail.com>
2020-09-29 15:07:41 +0900

    Rubocop_error_fix

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,11 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   has_many :images
-  #(considering) accepts_nested_attributes_for :images, allow_destroy: true 
+  # (considering) accepts_nested_attributes_for :images, allow_destroy: true 
   belongs_to :seller, class_name: 'User', foreign_key: 'seller_id'
   belongs_to :buyer, class_name: 'User', optional: true, foreign_key: 'buyer_id'
   belongs_to :category
-  #(considering) accepts_nested_attributes_for :category
+  # (considering) accepts_nested_attributes_for :category
   belongs_to :brand
   accepts_nested_attributes_for :brand
   belongs_to_active_hash :condition
@@ -16,10 +16,9 @@ class Item < ApplicationRecord
   validates :images, presence: true
   validates :name, presence: true, length: { maximum: 40 }
   validates :detail, presence: true, length: { maximum: 1000 }
-  validates :price, presence: true, inclusion: 300..9999999
+  validates :price, presence: true, inclusion: 300..9_999_999
   validates :condition_id, presence: true
   validates :prefecture_id, presence: true
   validates :delivery_fee, presence: true
   validates :delivery_day, presence: true
-
 end
