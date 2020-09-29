da0640d1c47b8691f5171fc68e54ed52cd86b56e
KeiMichell <krockdale611@gmail.com>
2020-09-29 15:09:19 +0900

    Rubocop_error_fix
    error: trailing whitespace detected

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   has_many :images
-  # (considering) accepts_nested_attributes_for :images, allow_destroy: true 
+  # (considering) accepts_nested_attributes_for :images, allow_destroy: true
   belongs_to :seller, class_name: 'User', foreign_key: 'seller_id'
   belongs_to :buyer, class_name: 'User', optional: true, foreign_key: 'buyer_id'
   belongs_to :category
