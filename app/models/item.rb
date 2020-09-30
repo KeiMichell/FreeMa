8dfa50e8305aa3c879e1221a9a7568ca4ac60fe1
KeiMichell <krockdale611@gmail.com>
2020-09-30 18:43:32 +0900

    edit-item-model

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,9 +5,8 @@ class Item < ApplicationRecord
   belongs_to :seller, class_name: 'User', foreign_key: 'seller_id'
   belongs_to :buyer, class_name: 'User', optional: true, foreign_key: 'buyer_id'
   belongs_to :category
-  accepts_nested_attributes_for :category
   belongs_to :brand
-  accepts_nested_attributes_for :brand
+  accepts_nested_attributes_for :brand, allow_destroy: true
   belongs_to_active_hash :condition
   belongs_to_active_hash :delivery_fee
   belongs_to_active_hash :delivery_day
@@ -16,7 +15,7 @@ class Item < ApplicationRecord
   validates :images, presence: true
   validates :name, presence: true, length: { maximum: 40 }
   validates :detail, presence: true, length: { maximum: 1000 }
-  validates :price, presence: true, inclusion: 300..9_999_999
+  validates :price, presence: true, inclusion: { in: (300..9_999_999)}
   validates :condition_id, presence: true
   validates :prefecture_id, presence: true
   validates :delivery_fee, presence: true
