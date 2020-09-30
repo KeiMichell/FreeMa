f945d9fea97382014e7154601aa0a6e3da2a85f4
shi0280 <sho_ko504128@yahoo.co.jp>
2020-09-30 12:06:58 +0900

    edit-item-model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,11 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   has_many :images
-  # accepts_nested_attributes_for :images, allow_destroy: true 検討中
+  accepts_nested_attributes_for :images, allow_destroy: true 
   belongs_to :seller, class_name: 'User', foreign_key: 'seller_id'
   belongs_to :buyer, class_name: 'User', optional: true, foreign_key: 'buyer_id'
   belongs_to :category
-  # accepts_nested_attributes_for :category 検討中
+  accepts_nested_attributes_for :category
   belongs_to :brand
   accepts_nested_attributes_for :brand
   belongs_to_active_hash :condition
