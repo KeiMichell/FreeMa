b0ffc98289da77220356cbcd5a907cf596102324
KeiMichell <krockdale611@gmail.com>
2020-10-03 18:43:10 +0900

    test_code_WIP
    商品出品機能_テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,18 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+
   has_many :images
   accepts_nested_attributes_for :images, allow_destroy: true
   belongs_to :seller, class_name: 'User', foreign_key: 'seller_id'
   belongs_to :buyer, class_name: 'User', optional: true, foreign_key: 'buyer_id'
   belongs_to :category
-  belongs_to :brand
+  belongs_to :brand, optional: true
   accepts_nested_attributes_for :brand, allow_destroy: true
   belongs_to_active_hash :condition
   belongs_to_active_hash :delivery_fee
   belongs_to_active_hash :delivery_day
   belongs_to_active_hash :prefecture
+
 
   validates :images, presence: true
   validates :name, presence: true, length: { maximum: 40 }
@@ -18,6 +20,7 @@ class Item < ApplicationRecord
   validates :price, presence: true, inclusion: { in: (300..9_999_999) }
   validates :condition_id, presence: true
   validates :prefecture_id, presence: true
-  validates :delivery_fee, presence: true
-  validates :delivery_day, presence: true
+  validates :delivery_fee_id, presence: true
+  validates :delivery_day_id, presence: true
+  validates :category_id, presence: true
 end
---
+++ spec/factories/items.rb
@@ -0,0 +1,22 @@
+FactoryBot.define do
+
+  factory :item do
+    name              {"tomato"}
+    detail            {"aaa"}
+    price             {300}
+    category_id       {1}
+    condition_id      {1}
+    delivery_fee_id   {1}
+    prefecture_id     {1}
+    delivery_day_id   {1}
+    association :seller, factory: :user
+    after(:build) do |item|
+      item.images << build(:image)
+      parent = create(:category, name: "親", parent_id: nil)
+      child = create(:category, name: "子", parent_id: parent.id)
+      grand_child = create(:category, name: "孫", parent_id: child.id)
+      item.category_id = grand_child.id
+    end
+  end
+
+end
