8a352515d23d14110950e98171ac688b76628073
shi0280 <sho_ko504128@yahoo.co.jp>
2020-10-04 08:27:38 +0900

    edit-controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -49,6 +49,7 @@ class ItemsController < ApplicationController
   def set_item
     @item = Item.find(params[:id])
     return if @item.seller_id == current_user.id
+
     redirect_to action: :index
   end
 
