66fdd2287315744e508eb9c118e4289f04e4a68f
KeiMichell <krockdale611@gmail.com>
2020-10-04 14:36:20 +0900

    Rubocop_error_fix

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
 
   factory :user do
-    nickname              { Faker::Name.last_name} 
+    nickname              { Faker::Name.last_name }
     email                 { Faker::Internet.free_email }
     password              { "00000000" }
     password_confirmation { "00000000" }
