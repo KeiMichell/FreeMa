5a2e950bb9887c2036116cb7f01d7c639df57886
KeiMichell <krockdale611@gmail.com>
2020-10-05 18:05:22 +0900

    Create 20200923125209_create_items.rb

---
+++ db/migrate/20200923125209_create_items.rb
@@ -0,0 +1,19 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.timestamps
+      t.string :name, null: false
+      t.text :detail, null: false
+      t.integer :price, null: false
+      t.integer :condition_id, null: false
+      t.integer :delivery_fee_id, null: false
+      t.integer :prefecture_id, null: false
+      t.integer :delivery_day_id, null: false
+      t.references :brand, foreign_key: true
+      t.references :category, null: false, foreign_key: true
+      t.references :seller, null: false, foreign_key: {to_table: :users}
+      t.references :buyer, null: false, foreign_key: {to_table: :users}
+    end
+  end
+end
+H
