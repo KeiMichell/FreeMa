3b5079a4d4d78cc96a603bb3d3357daf3a50995a
shi0280 <sho_ko504128@yahoo.co.jp>
2020-10-11 19:27:47 +0900

    edit-routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     collection do
       get 'get_category_children', to: 'items#get_category_children', defaults: { format: 'json' }
       get 'get_category_grandchildren', to: 'items#get_category_grandchildren', defaults: { format: 'json' }
+    end
     member do
       get 'purchase'
     end
