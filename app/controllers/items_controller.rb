7e8b94d7e57eb8316d2edb50e7f9a81253848bb2
KeiMichell <krockdale611@gmail.com>
2020-10-11 18:06:17 +0900

    edit-category-feature
    商品編集画面にカテゴリ機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
   before_action :set_item, except: [:index, :new, :create, :show, :get_category_children, :get_category_grandchildren]
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_category, only: [:new, :edit, :create, :update, :destroy]
 
   def index
     @items = Item.includes(:images).order('created_at DESC')
@@ -14,12 +15,9 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
     @item.images.new
-    @category_parent_array = ["---"]
-    @category_parent_array = Category.where(ancestry: nil)
   end
 
   def create
-    @category_parent_array = Category.where(ancestry: nil)
     @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
@@ -29,11 +27,28 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @category_parent_array = Category.where(ancestry: nil)
+    grandchild_category = @item.category
+    child_category = @item.category.parent
+    parent_category = @item.category.parent.parent
+
+    @category_parent_array = []
+    Category.where(ancestry: nil).each do |parent|
+      @category_parent_array << parent
+    end
+
+    @category_children_array = []
+    Category.where(ancestry: child_category.ancestry).each do |children|
+      @category_children_array << children
+    end
+
+    @category_grandchildren_array = []
+    Category.where(ancestry: grandchild_category.ancestry).each do |grandchildren|
+      @category_grandchildren_array << grandchildren
+    end
+
   end
 
   def update
-    @category_parent_array = Category.where(ancestry: nil)
     if item_params[:images_attributes].nil?
       render :edit
     end
@@ -72,5 +87,9 @@ class ItemsController < ApplicationController
 
   def move_to_index
     redirect_to action: :index unless user_signed_in?
+  end
+
+  def set_category  
+    @category_parent_array = Category.where(ancestry: nil)
   end
 end
