f7f822b4de826b2b362b54ad36f2d293e2af2e47
shi0280 <sho_ko504128@yahoo.co.jp>
2020-10-13 17:52:34 +0900

    edit-space

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,8 +1,8 @@
 class Favorite < ApplicationRecord
-    # Association
+  # Association
   belongs_to :user
   belongs_to :item
 
   # Validation
-  validates_uniqueness_of :item_id,  scope: :user_id
+  validates_uniqueness_of :item_id, scope: :user_id
 end
