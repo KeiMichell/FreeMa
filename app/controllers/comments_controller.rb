02eb08a40516ea9861d5ad10544c5f930b8109b2
shi0280 <sho_ko504128@yahoo.co.jp>
2020-10-17 18:02:35 +0900

    edit-comment-function

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.create(comment_params)
     respond_to do |format|
-      format.html { redirect_to item_path(params[:item_id])  }
+      format.html { redirect_to item_path(params[:item_id]) }
       format.json
     end
   end
@@ -14,6 +14,7 @@ class CommentsController < ApplicationController
   end
 
   private
+
   def comment_params
     params.require(:comment).permit(:comment).merge(user_id: current_user.id, item_id: params[:item_id])
   end
