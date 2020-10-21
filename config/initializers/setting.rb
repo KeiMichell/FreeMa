5f3569464b9927046bd89005bd45a5c6bc3bae0c
chomchompanic <chomurashouta@gmail.com>
2020-10-21 16:39:24 +0900

    registrtion-creditcard

---
+++ config/initializers/setting.rb
@@ -0,0 +1,2 @@
+PAYJP_PUBLIC_KEY = 'pk_test_3ae9a2dfc10038c0229732f6'
+PAYJP_SECRET_KEY = 'sk_test_9d7972d20198fe4daf87dffa'
--- config/routes.rb
+++ config/routes.rb
@@ -19,4 +19,7 @@ Rails.application.routes.draw do
     get 'addresses', to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
   end
+
+  resources :cards, only: [:index, :new, :create, :destroy]
+
 end
