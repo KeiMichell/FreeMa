46049b396c2fb5aef6339f0f4b33eaf3136ad980
chomchompanic <chomurashouta@gmail.com>
2020-10-27 13:35:26 +0900

    create-test

---
+++ spec/models/card_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe Card do
+  describe '#create' do
+    # user_id,
+    it 'is valid with a customer_id and card_id'do
+      user = create(:user)
+      card = build(:card, user_id: user.id)
+      card.valid?
+      expect(card).to be_valid
+    end
+
+    it 'is valid without customer_id' do
+      user = create(:user)
+      card = build(:card, user_id: user.id, customer_id:"")
+      card.valid?
+      expect(card.errors[:customer_id]).to include("を入力してください")
+    end
+
+    it 'is invalid without card_id' do
+      user = create(:user)
+      card = build(:card, user_id: user.id, card_id: "")
+      card.valid?
+      expect(card.errors[:card_id]).to include("を入力してください")
+    end
+  end
+end
+
+
